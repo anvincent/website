672c00fa617c9f4180c7593e033ebaa15810912b
anvincent <ao.vincent@gmail.com>
2014-06-01 22:16:01 -0500

    add bundle

---
+++ src/Core/BlogBundle/Controller/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function indexAction()
+    {
+    }
+
+}
