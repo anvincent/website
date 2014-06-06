f26b1bc01f4d7171d571b9715edc663c4ac84a17
anvincent <ao.vincent@gmail.com>
2014-06-06 16:33:50 -0500

    build blog bundle

--- src/Core/BlogBundle/Controller/PageController.php
+++ src/Core/BlogBundle/Controller/PageController.php
@@ -11,4 +11,9 @@ class PageController extends Controller
     {
         return $this->render('CoreBlogBundle:Page:index.html.twig');
     }
+    
+    public function aboutAction()
+    {
+    	return $this->render('CoreBlogBundle:Page:about.html.twig');
+    }
 }
