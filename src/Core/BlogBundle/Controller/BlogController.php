7d1d001e798ffe5230742729887747dccb183fe9
anvincent <ao.vincent@gmail.com>
2014-06-07 21:58:13 -0500

    blog update

---
+++ src/Core/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,30 @@
+<?php
+// src/Blogger/BlogBundle/Controller/BlogController.php
+
+namespace Core\BlogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Blog controller.
+ */
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $blog = $em->getRepository('CoreBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('CoreBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
+}
