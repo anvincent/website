bb218638719f7e4c82ba0267a651c0066922a06c
anvincent <ao.vincent@gmail.com>
2014-06-08 19:32:57 -0500

    create new form

---
+++ src/Core/BlogBundle/Form/CommentType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Core\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CommentType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('user')
+            ->add('comment')
+            ->add('approved')
+            ->add('created')
+            ->add('updated')
+            ->add('blog')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Core\BlogBundle\Entity\Comment'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'core_blogbundle_comment';
+    }
+}
