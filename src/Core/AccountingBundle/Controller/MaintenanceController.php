81d940b030f5d2585bf45d4cad883dab07380789
anvincent <ao.vincent@gmail.com>
2015-02-03 10:16:49 -0600

    test

--- src/Core/AccountingBundle/Controller/MaintenanceController.php
+++ src/Core/AccountingBundle/Controller/MaintenanceController.php
@@ -161,11 +161,14 @@ class MaintenanceController extends Controller
 	{
 		$em = $this->getDoctrine()
 		->getManager();
+		echo '<p>$account_id: </p>';print_r($account_id);							//test
 		$chartmaster = $this->getChartmaster($account_id);
+		echo '<p>$chartmaster: </p>';print_r($chartmaster);							//test
 		if (!$chartmaster) {
 			throw $this->createNotFoundException('Unable to find this entity.');
 		}
 		$form = $this->createForm(new ChartmasterType(), $chartmaster);
+		echo '<p>$form: </p>';print_r($form);										//test
 		$request = $this->getRequest();
 		if ($request->getMethod() == 'POST') {
 			$form->bind($request);
