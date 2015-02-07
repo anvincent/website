dbabd829381c2aa259d256a189264496efb2b31b
anvincent <ao.vincent@gmail.com>
2015-02-07 14:04:56 -0600

    Signed-off-by: anvincent

--- src/Core/AccountingBundle/Form/AccountgroupsType.php
+++ src/Core/AccountingBundle/Form/AccountgroupsType.php
@@ -24,7 +24,7 @@ class AccountgroupsType extends AbstractType
             ->add('sequenceintb','text')
             ->add('parentgroupname','entity',array(
             		'class' => 'CoreAccountingBundle:Accountgroups',
-            		'property' => 'parentgroupname',
+            		'property' => 'groupname',
             		'required' => false
             ))
             ->add('Confirm','submit');
