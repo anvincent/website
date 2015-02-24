1fb8d0852cfd7eb037330bd570ebb725ed08c97f
anvincent <ao.vincent@gmail.com>
2015-02-24 13:47:27 -0600

    test

--- src/Core/AccountingBundle/Controller/MaintenanceController.php
+++ src/Core/AccountingBundle/Controller/MaintenanceController.php
@@ -464,9 +464,10 @@ class MaintenanceController extends Controller
 	{
 		$em = $this->getDoctrine()->getManager();
 		$today = new \DateTime('today',new \DateTimeZone('America/Chicago'));
-		die(var_dump($today));
+//		die(var_dump($today));
 		$periods = $em	->getRepository('CoreAccountingBundle:Periods')
 						->findOneBylastdateinperiod($today->format('Y-m-t'));
+		die(var_dump($periods));
 		if (!$periods) {
 			throw $this->createNotFoundException('Unable to find Fiscal Period.');
 		}
