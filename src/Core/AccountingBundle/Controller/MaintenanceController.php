6b77b7e10604666954bde719114f90bcf771e070
anvincent <ao.vincent@gmail.com>
2015-02-25 20:22:35 -0600

    test

--- src/Core/AccountingBundle/Controller/MaintenanceController.php
+++ src/Core/AccountingBundle/Controller/MaintenanceController.php
@@ -757,11 +757,10 @@ class MaintenanceController extends Controller
 		$budgetbyperiod = $this->getBudget('budgetactualpriorcurrentnextbyaccount',$account_id);
 		
 		
-	//	$tags = $this->getTags('1');
-		
-		var_dump($budgetbyperiod);
+		$tags = $this->getTags('1');
+		var_dump($tags);
 		echo "</br></br>";
-	//	var_dump($tags);
+		var_dump($budgetbyperiod);
 		
 		
 		var_dump($budgetbyperiod->getAccountcode());
