e02e55235228d466977f341064df0cdf35840f54
anvincent <ao.vincent@gmail.com>
2015-02-28 09:28:11 -0600

    test

--- src/Core/AccountingBundle/Controller/MaintenanceController.php
+++ src/Core/AccountingBundle/Controller/MaintenanceController.php
@@ -752,7 +752,7 @@ class MaintenanceController extends Controller
 					->getManager();
 		$budgetbyperiod = $this->getBudget('budgetactualpriorcurrentnextbyaccount2',$account_id);
 		foreach ($budgetbyperiod as $singlebudget) {
-			echo "</br>"; \Doctrine\Common\Util\Debug::dump($budgetbyperiod); echo "</br>"; die();
+			echo "</br>"; \Doctrine\Common\Util\Debug::dump($singlebudget); echo "</br>"; die();
 		}
 		
 		
