c54ac1a54044df50238975410338d9cce0c59dc2
anvincent <ao.vincent@gmail.com>
2015-02-28 11:36:19 -0600

    Signed-off-by: anvincent

--- src/Core/AccountingBundle/Controller/MaintenanceController.php
+++ src/Core/AccountingBundle/Controller/MaintenanceController.php
@@ -23,7 +23,7 @@ use Core\AccountingBundle\Form\AccountsectionType;
 use Core\AccountingBundle\Form\AccountgroupsType;
 use Core\AccountingBundle\Form\TagsType;
 use Core\AccountingBundle\Form\ImporttransdefnType;
-use Core\AccountingBundle\Form\BudgetType;
+use Core\AccountingBundle\Form\BudgetsType;
 use Core\AccountingBundle\Form\ChartdetailsType;
 
 class MaintenanceController extends Controller
