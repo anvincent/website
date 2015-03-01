4dfa76f6e4103995c5cb44b0cb3c118c3501ba4a
anvincent <ao.vincent@gmail.com>
2015-02-28 18:48:15 -0600

    Signed-off-by: anvincent

---
+++ src/Core/AccountingBundle/Controller/TransactionsController.php
@@ -0,0 +1,39 @@
+<?php
+// src/Core/AccountingBundle/Controller/PageController.php
+
+namespace Core\AccountingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+// add entities
+
+
+// add forms
+
+
+class TransactionsController extends Controller
+{
+	/* Main */
+	public function indexAction()
+	{
+		return $this->render('CoreAccountingBundle:Transactions:index.html.twig');
+	}	
+	
+	/* Manual Journal Entries - gltrans 
+	 * 		add
+	 * 		get
+	 * 		delete
+	 */
+	public function addManualTransactionAction($typeno)
+	{
+		if ($typeno == 0) {
+			// new transaction typeno, get typeno
+		} else {
+			// 
+		}
+	}
+	 
+}
