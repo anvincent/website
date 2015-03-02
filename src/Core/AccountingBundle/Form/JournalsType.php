f8e5a35ccd9a0a29d9b15059d4384d4ce5f013d4
anvincent <ao.vincent@gmail.com>
2015-03-01 18:09:39 -0600

    transactions buildout
    Signed-off-by: anvincent

--- src/Core/AccountingBundle/Form/JournalsType.php
+++ src/Core/AccountingBundle/Form/JournalsType.php
@@ -6,7 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Doctrine\ORM\EntityRepository;
-Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class JournalsType extends AbstractType
 {
