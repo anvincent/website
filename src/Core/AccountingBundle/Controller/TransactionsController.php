9aa1e190be6510ad8a1e5e71beecbd4fd214d509
anvincent <ao.vincent@gmail.com>
2015-03-20 18:48:22 -0500

    test

--- src/Core/AccountingBundle/Controller/TransactionsController.php
+++ src/Core/AccountingBundle/Controller/TransactionsController.php
@@ -262,7 +262,7 @@ class TransactionsController extends Controller
 		$end = $period[0]['period']+3;
 		$periods = array();
 		for ($start; $start <= $end; $start++) {
-			$thedate = $this->getTheLastDate($start);
+			$thedate = $this->getTheLastDate($start);\Doctrine\Common\Util\Debug::dump($thedate);die();
 			$periods[] = array($start,$thedate[0]);
 		}
 		return $periods;
