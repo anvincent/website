b4787009a5432c47fd4c55b2fd98ee8c72abf9bd
anvincent <ao.vincent@gmail.com>
2015-03-28 08:20:48 -0500

    transactions buildout

--- src/Core/AccountingBundle/Entity/Document.php
+++ src/Core/AccountingBundle/Entity/Document.php
@@ -1,6 +1,6 @@
 <?php 
 // src/AppBundle/Entity/Document.php
-namespace AppBundle\Entity;
+namespace Core\AccountingBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
