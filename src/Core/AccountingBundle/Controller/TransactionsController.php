3d11b7e0fcfc30a64f14660e429489dd8f4422cc
anvincent <ao.vincent@gmail.com>
2015-03-30 20:19:44 -0500

    test

--- src/Core/AccountingBundle/Controller/TransactionsController.php
+++ src/Core/AccountingBundle/Controller/TransactionsController.php
@@ -314,10 +314,10 @@ class TransactionsController extends Controller
 		
 		if ($request->getMethod() == 'POST') {
 			$fileObj = $form->getData()->getFile();
-			$dataheader = json_decode($importoption->getDataheaderdefn());
+			$dataheader = $importoption->getDataheaderdefn();
 			
 			echo 'dataheader</br>';
-			\Doctrine\Common\Util\Debug::dump($dataheader);echo '</br>';die();
+			\Doctrine\Common\Util\Debug::dump(json_decode($dataheader));echo '</br>';die();
 			
 			$beginIndicator = FALSE;
 			
