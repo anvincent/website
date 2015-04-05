b645db2df05932b006109fb588ddddb99e18139b
anvincent <ao.vincent@gmail.com>
2015-04-05 08:01:32 -0500

    test

--- src/Core/AccountingBundle/Controller/TransactionsController.php
+++ src/Core/AccountingBundle/Controller/TransactionsController.php
@@ -309,7 +309,7 @@ class TransactionsController extends Controller
 		$periodrangestart = $this->getBatchPeriods('start');
 		$periodrangeend = $this->getBatchPeriods('end');
 		$importoptions = $this->getImporttransdefin();
-		$documents = $this->getDocuments();\Doctrine\Common\Util\Debug::dump($documents);die();
+		$documents = $this->getDocuments();//\Doctrine\Common\Util\Debug::dump($documents);die();
 		return $this->render('CoreAccountingBundle:Transactions:batchmenushow.html.twig', array(
 				'periodrangestart' 	=> $periodrangestart,
 				'periodrangeend' 	=> $periodrangeend,
